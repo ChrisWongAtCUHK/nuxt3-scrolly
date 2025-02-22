49ace89c07fba055fe4b841914ec6ef59b20419a
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-22 15:30:22 +0800

    Use simple loop

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,8 @@
 <template>
   <ol>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
-    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
-    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
-    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
-    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
-    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li v-for="index in 100" :key="index">
+      <NuxtLink :href="`/session/${index}`">Thing {{ index }}</NuxtLink>
+    </li>
   </ol>
   <LazyNuxtPage />
 </template>
