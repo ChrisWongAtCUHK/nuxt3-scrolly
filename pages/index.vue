5fbe3e1990a57222f34c411eeda95de0af295d54
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-22 14:23:24 +0800

    Demo the scroll behavior

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <ol>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+    <li><NuxtLink :href="'/session/1'">Thing 1</NuxtLink></li>
+    <li><NuxtLink :href="'/session/2'">Thing 2</NuxtLink></li>
+    <li><NuxtLink :href="'/session/3'">Thing 3</NuxtLink></li>
+    <li><NuxtLink :href="'/session/4'">Thing 4</NuxtLink></li>
+    <li><NuxtLink :href="'/session/5'">Thing 5</NuxtLink></li>
+  </ol>
+  <LazyNuxtPage />
+</template>
---
+++ pages/index/session/[id].vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+import { watch } from "vue";
+
+const route = useRoute();
+const router = useRouter();
+</script>
+
+<template>
+  <div
+    :style="{
+      position: 'fixed',
+      top: '16px',
+      right: '16px',
+      backgroundColor: '#cccccc',
+      padding: '8px',
+    }"
+  >
+    Thing {{ route.params.id }}.
+    <button @click="router.push('/')">(Close overlay)</button>
+  </div>
+</template>
